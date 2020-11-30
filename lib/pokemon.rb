44dfc8a61a740198ffe6f9490e49a44e28f4d677
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-11-29 19:20:57 -0500

    commit tree 1018366ece80f6b3cb2c453564a71cdfb116f260

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+
+     attr_accessor :id, :name, :type, :db
+
+     def initialize(id:, name:, type:, db:)
+          @id = id
+          @name = name
+          @type = type
+          @db = db
+     end
+
+     def self.save(id:, name:, type:)
+          if self.id
+               self.update
+          else
+               sql = <<-SQL
+               INSERT INTO pokemon (name)
+               VALUES (?)
+               SQL
+               DB[:conn].execute(sql, self.id, self.name, self.type)
+               @id = DB[:conn].execute("SELECT * FROM pokemon WHERE name = ?")[0][0]
+          end
+     end
+
+
 end
